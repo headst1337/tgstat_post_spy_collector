f535f59639edfc1f4b909de203a88f39460f1b9c
headst1337 <headst1337@gmail.com>
2023-07-06 22:19:58 +0700

    parsing module changed: now it is synchronous for better performance + cosmetic changes to dashboard and downloaded exel table

--- app/parse/parse.py
+++ app/parse/parse.py
@@ -1,10 +1,9 @@
-import asyncio
 import datetime
 import re
 import time
-import aiohttp
+import requests
+import random
 from urllib.parse import urlencode
-from concurrent.futures import ThreadPoolExecutor
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from run import app
@@ -13,71 +12,70 @@ from app.models.post import Post
 from app.config import Config
 
 
-async def extract_filtered_data(text: str) -> str|None:
+def extract_filtered_data(text: str) -> str|None:
     card_pattern = r'\d{12,19}'
     crypto_pattern = r'(0x[a-fA-F0-9]{40}|[13][a-km-zA-HJ-NP-Z0-9]{26,35})'
     mono_pattern = r'https://send\.monobank\.ua/jar/[a-zA-Z0-9]+'
     patterns = [card_pattern, crypto_pattern, mono_pattern]
     result_data = ' '.join([match for pattern in patterns for match in re.findall(pattern, text)])
+    result_data = ' '.join(list(dict.fromkeys(result_data.split())))
     return result_data if len(result_data) > 0 else None
 
-async def fetch_from_api(session, offset, limit):
-    end_date = int(time.time())
-    start_date = int((datetime.datetime.now() - datetime.timedelta(days=3)).timestamp())
+def fetch_from_api(session, offset) -> None:
+    #end_date = int(time.time())
+    #start_date = int((datetime.datetime.now() - datetime.timedelta(days=3)).timestamp())
     base_url = 'https://api.tgstat.ru/posts/search'
     query_params = {
         'token': Config.TGSTAT_TOKEN,
         'q': Config.KEYWORDS,
         'extended': 1,
         'offset': offset,
-        'limit': limit,
+        'limit': 50,
         'country': 'ua',
         'language': 'ukrainian',
         'hideForwards': 1,
-        'startDate': start_date,
-        'endDate': end_date,
+        # 'startDate': start_date,
+        # 'endDate': end_date,
         'extendedSyntax': 1
     }
 
     url = base_url + '?' + urlencode(query_params)
-    async with session.get(url) as response:
-        data = await response.json()
-        posts = []
-        for item in data['response']['items']:
-            post_link = item['link']
-            group_link = next((channel['link'] for channel in data['response']['channels'] if channel['id'] == item['channel_id']), None)
-            body = item['text']
-            payment_data = await extract_filtered_data(item['text'])
-            if payment_data:
-                timestamp = item['date']
-                post = Post(post_link=post_link, group_link=group_link, body=body, payment_data=payment_data, timestamp=datetime.datetime.fromtimestamp(timestamp))
-                posts.append(post)
-            with ThreadPoolExecutor() as executor:
-                await asyncio.get_running_loop().run_in_executor(executor, save_objects, posts)
+    response = session.get(url)
+    data = response.json()
+    for item in data['response']['items']:
+        post_link = item['link']
+        group_link = next((channel['link'] for channel in data['response']['channels'] if channel['id'] == item['channel_id']), None)
+        body = item['text']
+        payment_data = extract_filtered_data(item['text'])
+        if payment_data:
+            timestamp = item['date']
+            post = Post(post_link=post_link, group_link=group_link, body=body, payment_data=payment_data, timestamp=datetime.datetime.fromtimestamp(timestamp))
+            save_object(post)
+        else: print("No payment data")
 
-def save_objects(posts):
+def save_object(post) -> None:
     with app.app_context():
         with db.session() as session:
-            filtered_posts = []
-            for post in posts:
-                payment_data_words = post.payment_data.split(' ')
-                existing_post = session.query(Post).filter((Post.body == post.body) | (Post.payment_data.in_(payment_data_words))).first()
-                if existing_post is None:
-                    filtered_posts.append(post)
-            if filtered_posts:
-                session.bulk_save_objects(filtered_posts)
+            existing_post = session.query(Post).filter((Post.body == post.body) | (Post.payment_data == post.payment_data)).first()
+            if existing_post is None:
+                session.add(post)
                 session.commit()
+                print("Post saved!")
+            else:
+                print("Post exists")
 
-async def fetch_data():
-    async with aiohttp.ClientSession() as session:
-        with ThreadPoolExecutor() as executor:
-            tasks = []
-            limit = 50
-            loop = asyncio.get_event_loop()
-            for offset in range(0, 100):
-                task = await loop.run_in_executor(executor, fetch_from_api, session, offset, limit)
-                tasks.append(task)
-            await asyncio.gather(*tasks)
+
+def fetch_data() -> None:
+    with requests.Session() as session:
+        unique_offset = set()
+        for _ in range(1):
+            random_offset = random.randint(0, 1000)
+            while random_offset in unique_offset:
+                random_offset = random.randint(0, 1000)
+                unique_offset.add(random_offset)
+            print(f"Offset: {random_offset}")
+            fetch_from_api(session, random_offset)
+
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=lambda: asyncio.run(fetch_data()), trigger="interval", days=3)
+scheduler.add_job(fetch_data, trigger="interval", days=3)
--- app/utils.py
+++ app/utils.py
@@ -1,23 +1,47 @@
 from io import BytesIO
 from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import Font, Alignment
 
 
 def generate_excel_report(posts):
     workbook = Workbook()
     sheet = workbook.active
     sheet['A1'] = 'ID'
-    sheet['B1'] = 'Post Link'
-    sheet['C1'] = 'Group Link'
-    sheet['D1'] = 'Body'
-    sheet['E1'] = 'Payment Data'
-    sheet['F1'] = 'Timestamp'
+    sheet['B1'] = 'Ссылка на пост'
+    sheet['C1'] = 'Ссылка на группу'
+    sheet['D1'] = 'Содержимое'
+    sheet['E1'] = 'Платежные данные'
+    sheet['F1'] = 'Дата'
+
+    bold_font = Font(bold=True)
+    for cell in sheet[1]:
+        cell.font = bold_font
+
     for row, post in enumerate(posts, start=2):
         sheet.cell(row=row, column=1, value=post.id)
         sheet.cell(row=row, column=2, value=post.post_link)
         sheet.cell(row=row, column=3, value=post.group_link)
         sheet.cell(row=row, column=4, value=post.body)
-        sheet.cell(row=row, column=5, value=post.payment_data)
+
+        payment_cell = sheet.cell(row=row, column=5, value=post.payment_data)
+        payment_cell.alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
+
         sheet.cell(row=row, column=6, value=post.timestamp)
+
+    # Auto-fit column width
+    for column in sheet.columns:
+        max_length = 0
+        column = [cell for cell in column]
+        for cell in column:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        adjusted_width = (max_length + 2)
+        sheet.column_dimensions[get_column_letter(column[0].column)].width = adjusted_width
+
     output = BytesIO()
     workbook.save(output)
     output.seek(0)
